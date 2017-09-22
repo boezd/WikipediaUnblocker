61d571fc4e4fe0bcfcb23f3849a78de44742b820
Berkay <berkay.ozdemir813@gmail.com>
2017-09-22 09:03:24 +0300

    a chrome extension that unblocks wikipedia
    a simple chrome extension that unblocks wikipedia access by redirecting wikipedia to 0wikipedia
    
    also available in chrome web store https://chrome.google.com/webstore/detail/jnahpipkgjlglkbbjcngmeflakgieoai/

---
+++ background.js
@@ -0,0 +1,9 @@
+// Written by Berkay O 
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        return {
+            redirectUrl: details.url.replace("wikipedia", "0wikipedia")
+        };
+    }, {
+        urls: ["*://*.wikipedia.org/*"]
+    }, ["blocking"]);
